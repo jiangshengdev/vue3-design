88bdf113925c8304e3583f9e854d00d4dc4954a3
jiangsheng <jiangshengdev@outlook.com>
2022-06-29 10:38:06 +0800

    reactive

---
+++ packages/reactivity/src/baseHandlers.ts
@@ -0,0 +1,14 @@
+import { ReactiveFlags, Target } from './reactive'
+
+export const mutableHandlers: ProxyHandler<object> = {
+  get(target: Target, key: string | symbol, receiver: object) {
+    if(key===ReactiveFlags.IS_REACTIVE){
+      return
+    }
+
+    return Reflect.get(target, key, receiver)
+  },
+  set(target: object, key: string | symbol, value: unknown, receiver: object): boolean {
+    return Reflect.set(target, key, value, receiver)
+  }
+}
--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,5 +1,27 @@
 import { isObject } from '@vue/shared'
+import { mutableHandlers } from './baseHandlers'
+
+export const enum ReactiveFlags {
+  IS_REACTIVE = '__v_isReactive'
+}
+
+export interface Target {
+  [ReactiveFlags.IS_REACTIVE]?: boolean
+}
+
+export const reactiveMap = new WeakMap<Target, any>()
 
 export function reactive(target: object) {
-  return isObject({})
+  if (!isObject(target)) {
+    return target
+  }
+
+  let existingProxy = reactiveMap.get(target)
+  if (existingProxy) {
+    return existingProxy
+  }
+
+  let proxy = new Proxy(target, mutableHandlers)
+  reactiveMap.set(target, proxy)
+  return proxy
 }
