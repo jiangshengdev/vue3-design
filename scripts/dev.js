90e2808e551c9fe1fba9f345d1587147914e8780
jiangsheng <jiangshengdev@outlook.com>
2022-06-29 08:50:38 +0800

    build

---
+++ scripts/dev.js
@@ -0,0 +1,44 @@
+// @ts-check
+const { build } = require('esbuild')
+const { resolve, relative } = require('path')
+const args = require('minimist')(process.argv.slice(2))
+
+const target = args._[0] || 'reactivity'
+const format = args.f || 'global'
+const pkg = require(resolve(__dirname, `../packages/${target}/package.json`))
+
+// resolve output
+const outputFormat = format.startsWith('global')
+  ? 'iife'
+  : format === 'cjs'
+  ? 'cjs'
+  : 'esm'
+
+const postfix = format.endsWith('-runtime')
+  ? `runtime.${format.replace(/-runtime$/, '')}`
+  : format
+
+const outfile = resolve(
+  __dirname,
+  `../packages/${target}/dist/${
+    target === 'vue-compat' ? `vue` : target
+  }.${postfix}.js`
+)
+const relativeOutfile = relative(process.cwd(), outfile)
+
+build({
+  entryPoints: [resolve(__dirname, `../packages/${target}/src/index.ts`)],
+  outfile,
+  bundle: true,
+  sourcemap: true,
+  format: outputFormat,
+  globalName: pkg.buildOptions?.name,
+  platform: format === 'cjs' ? 'node' : 'browser',
+  watch: {
+    onRebuild(error) {
+      if (!error) console.log(`rebuilt: ${relativeOutfile}`)
+    }
+  }
+}).then(() => {
+  console.log(`watching: ${relativeOutfile}`)
+})
