b70e6b3ed364c88013a4ba5f8140eea2846f1fef
Jiang Sheng <jiangshengdev@outlook.com>
2022-07-06 22:20:29 +0800

    computed

--- src/main.ts
+++ src/main.ts
@@ -3,35 +3,21 @@ import { computed, effect, reactive } from '@vue/reactivity'
 // eslint-disable-next-line no-restricted-globals
 let app = document.getElementById('app')!
 
-let data = {
+let state = reactive({
   firstname: 'foo',
   lastname: 'bar'
-}
-let state = reactive(data)
-console.log(state)
+})
 
-let fullName = computed({
-  get() {
-    console.log('GET')
-    return `${state.firstname} ${state.lastname}`
-  },
-  set(newValue: any) {
-    console.log('SET')
-    let [firstname, lastname] = newValue
-
-    state.firstname = firstname
-    state.lastname = lastname
-  }
+let fullName = computed(() => {
+  console.log('get')
+  return `${state.firstname} ${state.lastname}`
 })
 
 effect(() => {
-  console.log('RUN EFFECT')
+  console.log('run')
   app.innerHTML = fullName.value
 })
 
 setTimeout(() => {
   state.firstname = 'baz'
-  setTimeout(() => {
-    fullName.value = ['sheng', 'jiang']
-  }, 1000)
 }, 1000)
